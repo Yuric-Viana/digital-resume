229a514fb26d099cabf3436508602ef9a51b1500
Yuri Viana <ycviana@sga.pucminas.br>
2025-12-10 12:33:40 -0300

    inicializar projeto

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+export default function App() {
+  return (
+    <div className="w-full h-screen flex">
+      <aside className="w-full h-screen bg-amber-400 flex-1">
+
+      </aside>
+      <main className="w-full h-screen bg-amber-950 flex-3">
+        
+      </main>
+    </div>
+  )
+}
