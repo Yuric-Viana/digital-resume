3f6097d457f3ca6daa61d6774fa124fe5a03f943
Yuri Viana <ycviana@sga.pucminas.br>
2025-12-11 15:52:54 -0300

    refactor: adicionar novas propriedades a tipagem do componente de título

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -1,12 +1,15 @@
 type Props = {
     title: string
     color?: string
+    borderColor?: "#2D5166" | "#0D2139" 
+    weight?: "default" | "bold"
+    size?: number
 }
 
-export function SectionTitle({ title, color = "white" }: Props) {
+export function SectionTitle({ title, color = "white", weight = "default", borderColor = "#2D5166", size = 2 }: Props) {
     return (
         <div>
-            <h2 className={`uppercase text-xl md:text-2xl text-${color} tracking-widest border-b-2 border-[#2D5166] pb-1`}>{title}</h2>
+            <h2 className={`uppercase text-xl md:text-2xl font-${weight} text-${color} tracking-widest border-b-${size} border-[${borderColor}] border- pb-1`}>{title}</h2>
         </div>
     )
 }
