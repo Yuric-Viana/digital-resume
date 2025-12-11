13b346620952375696d289b67a3386317b026b37
Yuri Viana <ycviana@sga.pucminas.br>
2025-12-11 15:01:16 -0300

    refactor: adicionar cor a tipagem Props

--- src/components/SectionTitle.tsx
+++ src/components/SectionTitle.tsx
@@ -1,11 +1,12 @@
 type Props = {
     title: string
+    color?: string
 }
 
-export function SectionTitle({ title }: Props) {
+export function SectionTitle({ title, color = "white" }: Props) {
     return (
         <div>
-            <h2 className="uppercase text-xl md:text-2xl text-white tracking-widest border-b-2 border-[#2D5166] pb-1">{title}</h2>
+            <h2 className={`uppercase text-xl md:text-2xl text-${color} tracking-widest border-b-2 border-[#2D5166] pb-1`}>{title}</h2>
         </div>
     )
 }
