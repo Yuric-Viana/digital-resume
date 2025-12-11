ffd3bcddf859acc74ea61451e96e057db4163ede
Yuri Viana <ycviana@sga.pucminas.br>
2025-12-11 11:42:31 -0300

    feat: adicionar profile ao aside

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,25 +1,11 @@
-import profilePhoto from "../assets/profile-photoo.jpg"
+import { Profile } from "./Profile"
+import { Contact } from "./Contact"
 
 export function Sidebar() {
     return (
-        <aside className="h-screen bg-[#0D2139] flex flex-1 flex-col justify-between">
+        <aside className="h-screen bg-[#0D2139] flex flex-1 flex-col gap-9 px-4 md:flex-2 lg:flex-1 py-2 lg:py-0">
 
-            <div className="w-full flex flex-col justify-center items-center p-2 md:py-8">
-                
-                <div className="w-32 h-32 md:w-40 md:h-40 rounded-full bg-[#1d5c6e] p-2 md:p-3 flex items-center justify-center">
-                    <div className="w-full h-full rounded-full bg-[#0a3a4a] p-1 flex items-center justify-center">
-                        <img
-                            src={profilePhoto}
-                            alt="Foto de perfil"
-                            className="rounded-full w-full h-full object-cover"
-                        />
-                    </div>
-                </div>
-
-                <h1 className="font-bold text-center text-base md:text-2xl text-white pt-4">Yuri Cardoso Viana</h1>
-                <strong className="text-[#d3e4ed] text-center text-xs md:text-base font-light">Engenherio de Software em formação</strong>
-            </div>
-
+            <Profile />
         </aside>
     )
 }
