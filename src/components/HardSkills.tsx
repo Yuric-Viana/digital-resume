b0b6e848a65ce10a3ec64fa34a70bf8239794ec5
Yuri Viana <ycviana@sga.pucminas.br>
2025-12-12 12:53:22 -0300

    feat: adicionar efeitos nos cards de hard skills

--- src/components/HardSkills.tsx
+++ src/components/HardSkills.tsx
@@ -30,20 +30,20 @@ export function HardSkills() {
             <div className="pt-4.5 grid gap-8">
                 <div className="pt-2">
                     <h4 className="text-xl font-semibold border-l-6 border-[#0A92AA] pl-2">Linguagens de Programação e Marcação</h4>
-                    <ul className="pt-4 lg:pl-12 grid gap-2.5 md:grid-cols-2 lg:grid-cols-4">
-                        <li className="bg-[#EFE5EA] p-2 rounded-4xl w-auto px-6 flex gap-2 justify-center">
+                    <ul className="pt-4 lg:pl-12 grid gap-4.5 md:grid-cols-2 lg:grid-cols-4">
+                        <li className=" cursor-pointer transition-all scale-100 hover:scale-110 bg-[#EFE5EA] p-2 rounded-4xl w-auto px-6 flex gap-2 justify-center duration-400 hover:text-white shadow-lg card-fill-effect-right ease-linear">
                             <img className="w-6" src={html} alt="Ícone do HTML" />
                             <p className="font-bold">HTML</p>
                         </li>
-                        <li className="bg-[#EFE5EA] p-2 rounded-4xl w-auto px-6 flex gap-2 justify-center">
+                        <li className=" cursor-pointer scale-100 transition-transform hover:scale-110 duration-400 hover:text-white shadow-lg card-fill-effect-right ease-linear bg-[#EFE5EA] p-2 rounded-4xl w-auto px-6 flex gap-2 justify-center">
                             <img className="w-6" src={css} alt="Ícone do CSS" />
                             <p className="font-bold">CSS</p>
                         </li>
-                        <li className="bg-[#EFE5EA] p-2 rounded-4xl flex gap-2 justify-center">
+                        <li className=" cursor-pointer scale-100 transition-transform hover:scale-110 duration-400 hover:text-white shadow-lg card-fill-effect-right ease-linear bg-[#EFE5EA] p-2 rounded-4xl flex gap-2 justify-center">
                             <img className="w-6" src={js} alt="Ícone do JavaScript" />
                             <p className="font-bold">JavaScript</p>
                         </li>
-                        <li className="bg-[#EFE5EA] p-2 rounded-4xl flex gap-2 justify-center">
+                        <li className=" cursor-pointer scale-100 transition-transform hover:scale-110 duration-400 hover:text-white shadow-lg card-fill-effect-right ease-linear bg-[#EFE5EA] p-2 rounded-4xl flex gap-2 justify-center">
                             <img className="w-6" src={ts} alt="Ícone do TypeScript" />
                             <p className="font-bold">Type Script</p>
                         </li>
@@ -52,26 +52,26 @@ export function HardSkills() {
 
                 <div className="pt-2">
                     <h4 className="text-xl font-semibold border-l-6 border-[#0A92AA] pl-2">Framework e Bibliotecas</h4>
-                    <ul className="pt-6 lg:pl-12 grid md:grid-cols-2 lg:grid-cols-4 gap-2.5">
-                        <li className="bg-[#EFE5EA] p-2 rounded-4xl w-auto px-6 flex gap-2 justify-center">
+                    <ul className="pt-6 lg:pl-12 grid md:grid-cols-2 lg:grid-cols-4 gap-4  .5">
+                        <li className=" cursor-pointer scale-100 transition-transform hover:scale-110 duration-400 hover:text-white shadow-lg card-fill-effect-left bg-[#EFE5EA] p-2 rounded-4xl w-auto px-6 flex gap-2 justify-center">
                             <img className="w-6" src={express} alt="Ícone do Express" />
                             <p className="font-bold">Express</p>
                         </li>
 
-                        <li className="bg-[#EFE5EA] p-2 rounded-4xl w-auto px-6 flex gap-2 justify-center">
+                        <li className=" cursor-pointer scale-100 transition-transform hover:scale-110 duration-400 hover:text-white shadow-lg card-fill-effect-left bg-[#EFE5EA] p-2 rounded-4xl w-auto px-6 flex gap-2 justify-center">
                             <img className="w-6" src={tailwind} alt="Ícone do Tailwind" />
                             <p className="font-bold">Tailwind</p>
                         </li>
 
-                        <li className="bg-[#EFE5EA] p-2 rounded-4xl w-auto px-6 flex gap-2 justify-center">
+                        <li className=" cursor-pointer scale-100 transition-transform hover:scale-110 duration-400 hover:text-white shadow-lg card-fill-effect-left bg-[#EFE5EA] p-2 rounded-4xl w-auto px-6 flex gap-2 justify-center">
                             <img className="w-6" src={react} alt="Ícone do React" />
                             <p className="font-bold">React</p>
                         </li>
-                        <li className="bg-[#EFE5EA] p-2 rounded-4xl w-auto px-6 flex gap-2 justify-center">
+                        <li className=" cursor-pointer scale-100 transition-transform hover:scale-110 duration-400 hover:text-white shadow-lg card-fill-effect-left bg-[#EFE5EA] p-2 rounded-4xl w-auto px-6 flex gap-2 justify-center">
                             <img className="w-6" src={prisma} alt="Ícone do Prisma" />
                             <p className="font-bold">Prisma</p>
                         </li>
-                        <li className="bg-[#EFE5EA] p-2 rounded-4xl w-auto px-6 flex gap-2 justify-center">
+                        <li className=" cursor-pointer scale-100 transition-transform hover:scale-110 duration-400 hover:text-white shadow-lg card-fill-effect-left bg-[#EFE5EA] p-2 rounded-4xl w-auto px-6 flex gap-2 justify-center">
                             <img className="w-6" src={jest} alt="Ícone do Jest" />
                             <p className="font-bold">Jest</p>
                         </li>
@@ -80,22 +80,22 @@ export function HardSkills() {
 
                 <div className="pt-2">
                     <h4 className="text-xl font-semibold border-l-6 border-[#0A92AA] pl-2">Ferramentas e IDEs</h4>
-                    <ul className="pt-6 lg:pl-12 grid md:grid-cols-2 lg:grid-cols-4 gap-2.5">
-                        <li className="bg-[#EFE5EA] p-2 rounded-4xl w-auto px-6 flex gap-2 justify-center">
+                    <ul className="pt-6 lg:pl-12 grid md:grid-cols-2 lg:grid-cols-4 gap-4  .5">
+                        <li className=" cursor-pointer scale-100 transition-transform hover:scale-110 duration-400 hover:text-white shadow-lg card-fill-effect-right bg-[#EFE5EA] p-2 rounded-4xl w-auto px-6 flex gap-2 justify-center">
                             <img className="w-6" src={git} alt="Ícone do Git" />
                             <p className="font-bold">Git</p>
                         </li>
 
-                        <li className="bg-[#EFE5EA] p-2 rounded-4xl w-auto px-6 flex gap-2 justify-center">
+                        <li className=" cursor-pointer scale-100 transition-transform hover:scale-110 duration-400 hover:text-white shadow-lg card-fill-effect-right bg-[#EFE5EA] p-2 rounded-4xl w-auto px-6 flex gap-2 justify-center">
                             <img className="w-6" src={github} alt="Ícone do Git Hub" />
                             <p className="font-bold">Git Hub</p>
                         </li>
                         
-                        <li className="bg-[#EFE5EA] p-2 rounded-4xl w-auto px-6 flex gap-2 justify-center">
+                        <li className=" cursor-pointer scale-100 transition-transform hover:scale-110 duration-400 hover:text-white shadow-lg card-fill-effect-right bg-[#EFE5EA] p-2 rounded-4xl w-auto px-6 flex gap-2 justify-center">
                             <img className="w-6" src={intellij} alt="Ícone do Intellij" />
                             <p className="font-bold">Intellij</p>
                         </li>
-                        <li className="bg-[#EFE5EA] p-2 rounded-4xl w-auto px-6 flex gap-2 justify-center">
+                        <li className=" cursor-pointer scale-100 transition-transform hover:scale-110 duration-400 hover:text-white shadow-lg card-fill-effect-right bg-[#EFE5EA] p-2 rounded-4xl w-auto px-6 flex gap-2 justify-center">
                             <img className="w-6" src={vsCode} alt="Ícone do Jest" />
                             <p className="font-bold">VS Code</p>
                         </li>
@@ -104,27 +104,27 @@ export function HardSkills() {
 
                 <div className="pt-2">
                     <h4 className="text-xl font-semibold border-l-6 border-[#0A92AA] pl-2">Ferramentas de Build, Runtime e Virtualização</h4>
-                    <ul className="pt-6 lg:pl-12 grid md:grid-cols-2 lg:grid-cols-4 gap-2.5">
-                        <li className="bg-[#EFE5EA] p-2 rounded-4xl w-auto px-6 flex gap-2 justify-center">
+                    <ul className="pt-6 lg:pl-12 grid md:grid-cols-2 lg:grid-cols-4 gap-4  .5">
+                        <li className=" cursor-pointer scale-100 transition-transform hover:scale-110 duration-400 hover:text-white shadow-lg card-fill-effect-left bg-[#EFE5EA] p-2 rounded-4xl w-auto px-6 flex gap-2 justify-center">
                             <img className="w-6" src={docker} alt="Ícone do Docker" />
                             <p className="font-bold">Docker</p>
                         </li>
 
-                        <li className="bg-[#EFE5EA] p-2 rounded-4xl w-auto px-6 flex gap-2 justify-center">
+                        <li className=" cursor-pointer scale-100 transition-transform hover:scale-110 duration-400 hover:text-white shadow-lg card-fill-effect-left bg-[#EFE5EA] p-2 rounded-4xl w-auto px-6 flex gap-2 justify-center">
                             <img className="w-6" src={builder} alt="Ícone do Webpack" />
                             <p className="font-bold">Webpack</p>
                         </li>
 
-                        <li className="bg-[#EFE5EA] p-2 rounded-4xl w-auto px-6 flex gap-2 justify-center">
+                        <li className=" cursor-pointer scale-100 transition-transform hover:scale-110 duration-400 hover:text-white shadow-lg card-fill-effect-left bg-[#EFE5EA] p-2 rounded-4xl w-auto px-6 flex gap-2 justify-center">
                             <img className="w-6" src={vite} alt="Ícone do Vite" />
                             <p className="font-bold">Vite</p>
                         </li>
                         
-                        <li className="bg-[#EFE5EA] p-2 rounded-4xl w-auto px-6 flex gap-2 justify-center">
+                        <li className=" cursor-pointer scale-100 transition-transform hover:scale-110 duration-400 hover:text-white shadow-lg card-fill-effect-left bg-[#EFE5EA] p-2 rounded-4xl w-auto px-6 flex gap-2 justify-center">
                             <img className="w-6" src={node} alt="Ícone do Node js" />
                             <p className="font-bold">Node.js</p>
                         </li>
-                        <li className="bg-[#EFE5EA] p-2 rounded-4xl flex gap-2 justify-center">
+                        <li className=" cursor-pointer scale-100 transition-transform hover:scale-110 duration-400 hover:text-white shadow-lg card-fill-effect-left bg-[#EFE5EA] p-2 rounded-4xl flex gap-2 justify-center">
                             <img className="w-6" src={database} alt="Ícone de banco de dados" />
                             <p className="font-bold">Banco de dados</p>
                         </li>
