d3582d89255a75b2cd742b985e50e79d6e3689ba
Yuri Viana <ycviana@sga.pucminas.br>
2025-12-12 19:08:56 -0300

    refactor: remover efeito do card de hard skills

--- src/components/HardSkillsComponent.tsx
+++ src/components/HardSkillsComponent.tsx
@@ -16,7 +16,7 @@ export function HardSkillsComponent({ skills, title }: HardSkillsProps) {
             <ul className="pt-4 lg:pl-12 grid gap-4.5 md:grid-cols-2 lg:grid-cols-4">
                 {
                     skills.map((skill) => (
-                        <li className=" cursor-pointer transition-all scale-100 lg:hover:scale-110 bg-[#EFE5EA] p-2 rounded-4xl w-auto px-6 flex gap-2 justify-center duration-400 hover:text-white shadow-lg card-fill-effect ease-linear">
+                        <li className=" cursor-pointer transition-all scale-100 lg:hover:scale-110 bg-[#EFE5EA] p-2 rounded-4xl w-auto px-6 flex gap-2 justify-center ease-linear">
                             <img className="w-6" src={skill.iconSrc} alt={skill.iconAlt} />
                             <p className="font-bold">{skill.name}</p>
                         </li>
